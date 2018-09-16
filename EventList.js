946a003b7916ed222b6faa0de5865d9081570f1b
monbo <monbo@inbox.lt>
2018-09-16 22:37:56 +0300

    Added Eventlist

---
+++ EventList.js
@@ -0,0 +1,14 @@
+import React, { Component } from "react";
+import { FlatList, Text } from "react-native";
+
+class EventList extends Component {
+  render() {
+    return (
+      <FlatList
+        data={[{ name: "a" }, { name: "b" }]}
+        renderItem={({ item }) => <Text>{item.name}</Text>}
+      />
+    );
+  }
+}
+export default EventList;
