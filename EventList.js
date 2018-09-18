0414ff5efc381aa5d3b2d5678eb7b80747fa8942
monbo <monbo@inbox.lt>
2018-09-18 20:27:45 +0300

    Included Event Card class

--- EventList.js
+++ EventList.js
@@ -1,14 +1,50 @@
 import React, { Component } from "react";
-import { FlatList, Text } from "react-native";
+import { FlatList, Text, StyleSheet } from "react-native";
+
+import EventCard from './EventCard';
+
+const styles = StyleSheet.create({
+  list: {
+    flex: 1,
+    paddingTop: 20,
+    backgroundColor: '#ADD8E6'
+  }
+});
 
 class EventList extends Component {
+
+  state = {
+    events: []
+  }
+
+  componentDidMount() {
+    setInterval(() => {
+         this.setState({
+           events: this.state.events.map(evt => ({
+             ...evt,
+             timer: Date.now(),
+           })),
+         });
+       }, 1000);
+    
+    const events = require("./db.json").events.map(e => ({
+      ...e,
+      date: new Date(e.date),
+    }));
+    this.setState({ events });
+  }
+
   render() {
     return (
       <FlatList
-        data={[{ name: "a" }, { name: "b" }]}
-        renderItem={({ item }) => <Text>{item.name}</Text>}
+        style={styles.list}
+        data={this.state.events}
+        renderItem={({ item }) => <EventCard event={item} />}
+        keyExtractor = {item => item.id}
       />
     );
   }
 }
+
+
 export default EventList;
